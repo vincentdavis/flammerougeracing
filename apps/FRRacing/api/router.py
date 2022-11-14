7ef7478351042923452d3d059a99fab7255eeb54
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-11-15 00:35:25 +0530

    Initial code

--- apps/FRRacing/api/router.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.FRRacing.api import viewset
-
-router = DefaultRouter()
-router.register('TTTLeagueStandings',viewset.TTTLeagueStandings )
-router.register('Tttstageresrteam',viewset.Tttstageresrteam )
-router.register('Tttriderstats',viewset.Tttriderstats )
-router.register('Tttstagesegtsprint',viewset.Tttstagesegtsprint )
--- deployment/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker build -t frr-image:prd -f Dockerfile .
-if [ `echo $?` == 0 ]; then
-	docker rm -f frr
-	docker run -dt --restart=always -p 8008:8008 --name frr frr-image:prd
-fi
