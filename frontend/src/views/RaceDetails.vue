99555590bfa1170cc9251af6e90530dd3e6ed5c9
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2022-11-26 22:57:54 +0530

    Initial code

--- frontend/src/views/RaceDetails.vue
+++ frontend/src/views/RaceDetails.vue
@@ -4,7 +4,7 @@
           <v-img
             class="ma-0 pa-0"
             contain
-            src="https://ts-production.imgix.net/images/9ced2ef0-d902-4672-8def-e23a4d57303d.jpg?auto=compress,format&w=800&h=450"
+            :src="race_detail.logo"
           >
             <v-app-bar flat color="rgba(0, 0, 0, 0)">
               <v-spacer></v-spacer>
